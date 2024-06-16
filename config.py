70afa5f361fcc800dfb54cbf761951fd2925470e
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-06-16 08:54:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002229216652"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://aayushraj0281:UDgE6cYzPVNGlV0W@cluster0.ht9qb6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "aayushraj0281")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQCd9glgtqlqr8CcPs8AksC8x9Q2XNlD2fSnTLa959-25qwbiANCCDy_yWLxqWNPFwVgdJVKVR-aK_pIqbgO9nGC1dh5X_7u7-2ox56j9EyUU8ob0EnZQ2jl6a0ltm7IPozlI3Q_3n2O4ja0f09uXZELaWe2oNuNN3MRh8FSmVHnx1_lbBfntgkV44RpALv56VQ8xFcVM2Ugjb6bO6qveMpj3L483UqL3VXjCnveBtwZ5XKfyCt7zZvrpKAS5TBGdaft8ZFgEiIjK5UCQGd1yvtdtaPDqYtRMPM27kkhJIjnqY4esgltqNwvUGrozVylH5oOzERFdN6z2Cf9j0xlAEAAAAAT1ieooA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
 
 
