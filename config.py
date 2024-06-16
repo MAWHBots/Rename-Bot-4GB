c9a6c42de0f9bdf63a23a2738b232efd45b18c5a
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-06-16 08:38:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7428408058:AAEZ9o-hDt4U5UYj5e5r7cmz8TDgDWqS9Jo")
+API_ID = int(os.environ.get("API_ID", "23323912"))
+API_HASH = os.environ.get("API_HASH", "5b7decd292e78a57d631ea1849b7098a")
+OWNER = int(os.environ.get("OWNER", "5324831370"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "CelestialButcherBot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "mawhOrzzBackUp2")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002229216652"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://aayushraj0281:UDgE6cYzPVNGlV0W@cluster0.ht9qb6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "aayushraj0281")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
