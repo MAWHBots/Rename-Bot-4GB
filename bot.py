6be22ea404111b1eda546b4b191462f264b5c37f
MAWHBots <160210473+MAWHBots@users.noreply.github.com>
2024-06-19 12:13:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from plugins.cb_data import app as Client2
 from config import *
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+pyrogram.utils.MIN_CHANNEL_ID = -1002229216652
 
 
 bot = Client(
